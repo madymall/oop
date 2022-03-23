b990ede7207f7f44200c5c7d0d6da815e4a6931b
madymall <100951935+madymall@users.noreply.github.com>
2022-03-23 17:35:31 +0600

    Create homework6.py

---
+++ homeworks_oop/homework6.py
@@ -0,0 +1,26 @@
+def get_list() -> list:
+    return list(range(0, 1_000_000_000, 2))
+
+class Solution:
+
+    def find_target(self, list, target):
+        steps = 0
+        start = 0
+        end = len(list)
+
+        while start < end:
+            center = (start + end) // 2
+            steps += 1
+
+            if target == center:
+                return steps
+
+            elif target > center:
+                start = center + 1
+
+            else:
+                end = center - 1
+        return steps
+
+a = Solution().find_target(get_list(), 2_345_678)
+print(a)
